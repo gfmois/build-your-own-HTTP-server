ac7fa43d6567b189d3bac107e1a34ea55d45db8f
Moisés Guerola <daw.moisesguerola@gmail.com>
2025-08-16 10:58:30 +0200

    add: log4j and created simple http server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,22 @@
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.apache.logging.log4j.LogManager;
+
 public class Main {
+  private final static Logger logger = LogManager.getLogger(Main.class);
+
   public static void main(String[] args) {
-    // Uncomment this block to pass the first stage
-    //
+    logger.info("Starting server");
 
-    System.out.println("Hello World");
-
-    // try {
-    //   ServerSocket serverSocket = new ServerSocket(4221);
-    //
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //
-    //   serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
+    try (ServerSocket serverSocket = new ServerSocket(4221)) {
+      serverSocket.setReuseAddress(true);
+      serverSocket.accept(); // Wait for connection from client.
+      logger.info("accepted new connection");
+    } catch (IOException e) {
+      logger.error("IOException: " + e.getMessage());
+    }
   }
 }

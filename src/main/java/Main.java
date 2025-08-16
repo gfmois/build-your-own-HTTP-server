3cb4b42ae517a582de90d48239df766db25e6645
Moisés Guerola <daw.moisesguerola@gmail.com>
2025-08-16 11:04:44 +0200

    feat: http server respond with 200 status code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
-import org.apache.logging.log4j.Logger;
-
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
   private final static Logger logger = LogManager.getLogger(Main.class);
@@ -13,10 +14,20 @@ public class Main {
 
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
       serverSocket.setReuseAddress(true);
-      serverSocket.accept(); // Wait for connection from client.
-      logger.info("accepted new connection");
+      
+      try (Socket clientSocket = serverSocket.accept()) {
+        logger.info("accepted new connection");
+        
+        // InputStreamReader input = new InputStreamReader(clientSocket.getInputStream());
+        OutputStreamWriter output = new OutputStreamWriter(clientSocket.getOutputStream());
+
+        output.write("HTTP/1.1 200 OK\r\n\r\n");
+        output.flush();
+      } catch (IOException e) {
+        logger.error("IOException: {}", e.getMessage());
+      }      
     } catch (IOException e) {
-      logger.error("IOException: " + e.getMessage());
+      logger.error("IOException: {}", e.getMessage());
     }
   }
 }

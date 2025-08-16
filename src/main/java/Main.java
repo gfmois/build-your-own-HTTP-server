11ab22a1744826b3d7bcffe41946639ed04d420e
Moisés Guerola <daw.moisesguerola@gmail.com>
2025-08-16 10:45:50 +0200

    bugfix: java 23 to 21

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
     //
+
+    System.out.println("Hello World");
+
     // try {
     //   ServerSocket serverSocket = new ServerSocket(4221);
     //
